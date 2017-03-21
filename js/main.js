0b946edc825aea4185b611345446baa6063e46a0
PauerkrautSauerkraut <pauerkraut1@gmail.com>
2017-03-21 16:23:32 -0700

    Update main.js

--- js/main.js
+++ js/main.js
@@ -109,7 +109,7 @@ function create() {
 //              'down' : Phaser.KeyCode.S, 'left' : Phaser.KeyCode.A,
 //              'right' : Phaser.KeyCode.D});
 
-    //game.camera.follow(seedling);
+    game.camera.follow(seedling);
 
 }
 
@@ -174,8 +174,8 @@ function update() {
     {
         seedling.body.velocity.y = -150;
     }
-        game.camera.x = seedling.x;
-        game.camera.y = seedling.y;
+        //game.camera.x = seedling.x;
+        //game.camera.y = seedling.y;
         console.log(seedling.x + "This is x of seedling");
         console.log(seedling.y + "This is y of seedling");
         console.log(game.camera.x + "This is the game camera");
